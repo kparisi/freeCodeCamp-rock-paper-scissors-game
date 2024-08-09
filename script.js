2ec3c2200df1a90ae941349a211e5ac8f77ed702
kparisi <kristenparisi3@gmail.com>
2024-08-09 09:26:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function getRandomComputerResult() {
+  const options = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
+}
 
+let playerScore = 0;
+let computerScore = 0;
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+ 
+}
+
+console.log(getRoundResults("Rock"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
